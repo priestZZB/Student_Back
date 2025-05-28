4cd27d2bf7189f9f338c1c06e6a8ea71b17aa7e9
priestZZB <3389329200@qq.com>
2025-05-28 17:51:09 +0800

    修改配置

--- src/main/java/zzb/StudentBackApplication.java
+++ src/main/java/zzb/StudentBackApplication.java
@@ -1,16 +1,21 @@
 package zzb;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("zzb.dao")
-public class StudentBackApplication {
+public class StudentBackApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StudentBackApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudentBackApplication.class, args);
     }
-
 }
